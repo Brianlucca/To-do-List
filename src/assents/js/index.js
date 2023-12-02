037aa8498eaa07ae9077a5656e65f40fdfd26f81
Brian Lucca <brianlucca2002@gmail.com>
2023-12-02 11:38:17 -0300

    Ajustando LocalStorag, agora funcional

--- src/assents/js/index.js
+++ src/assents/js/index.js
@@ -1,6 +1,35 @@
 const criaStorage = () => {
-    window.localStorage.setItem('titulo', 'lista')
-    alert('ola')
-}
+  const dadosLista = {
+    titulo: input.value,
+    list_todo: textarea.value,
+  };
 
-document.getElementById('submit').onclick = criaStorage;
+  window.localStorage.setItem("Lista", JSON.stringify(dadosLista));
+};
+const getStorage = () => {
+  const dadosLista = window.localStorage.getItem("Lista");
+
+  if (dadosLista) {
+    const objDadosLista = JSON.parse(dadosLista);
+    const { titulo, list_todo } = objDadosLista;
+
+    const resumo = titulo + "<br>" + list_todo;
+    document.getElementById("resultado").innerHTML = resumo;
+  }
+};
+
+const removeStorage = () => {
+  window.localStorage.removeItem("Lista");
+};
+
+const limpaStorage = () => {
+  window.localStorage.clear();
+};
+
+document.getElementById("submit").onclick = criaStorage;
+document.getElementById("remove-storage").onclick = removeStorage;
+document.getElementById("limpa-storage").onclick = limpaStorage;
+const input = document.getElementById("title-Todo");
+const textarea = document.getElementById("list-to-do");
+
+getStorage();
