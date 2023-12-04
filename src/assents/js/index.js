7c991f925d03f4c991f9dca93ea10b901f6274d6
Brian Lucca <brianlucca2002@gmail.com>
2023-12-04 02:44:13 -0300

    Adicionando funções para o to-do-list

--- src/assents/js/index.js
+++ src/assents/js/index.js
@@ -1,35 +1,61 @@
-const criaStorage = () => {
-  const dadosLista = {
-    titulo: input.value,
-    list_todo: textarea.value,
-  };
+const input = document.querySelector("#list-to-do");
+const btn = document.querySelector("#submit-list");
+const div = document.querySelector("#resultado");
 
-  window.localStorage.setItem("Lista", JSON.stringify(dadosLista));
-};
-const getStorage = () => {
-  const dadosLista = window.localStorage.getItem("Lista");
-
-  if (dadosLista) {
-    const objDadosLista = JSON.parse(dadosLista);
-    const { titulo, list_todo } = objDadosLista;
-
-    const resumo = titulo + "<br>" + list_todo;
-    document.getElementById("resultado").innerHTML = resumo;
+input.addEventListener("keypress", (e) => {
+  if (e.keyCode == 13) {
+    let tarefa = {
+      nome: input.value,
+      id: gerarID(),
+    }
+    adicionarLista(tarefa)
   }
+});
+
+btn.addEventListener('click', (e) => {
+  let tarefa = {
+    nome: input.value,
+    id: gerarID(),
+  }
+  adicionarLista(tarefa)
+});
+
+const gerarID = () => Math.floor(Math.random() * 3000);
+
+const adicionarLista = (tarefa) => {
+  let li = criarTagLi(tarefa);
+  resultado.appendChild(li);
+  input.value = ''
 };
 
-const removeStorage = () => {
-  window.localStorage.removeItem("Lista");
+const criarTagLi = (tarefa) => {
+  let li = document.createElement('li')
+
+  let span = document.createElement('span')
+  span.classList.add('text-list')
+  span.innerHTML = ' <input type="checkbox" id="meuCheckbox" name="meuCheckbox" value="aceito">'+ tarefa.nome
+
+  let div = document.createElement('div')
+
+  let btnEditar = document.createElement('button')
+  btnEditar.classList.add('btnAcao')
+  btnEditar.innerHTML = '<i class="fa fa-pencil"></i>'
+  btnEditar.setAttribute('onclick', 'editar('+tarefa.id+')')
+
+  let btnExcluir = document.createElement('button')
+  btnExcluir.classList.add('btnAcao')
+  btnExcluir.innerHTML = '<i class="fa fa-trash"></i>'
+  btnExcluir.setAttribute('onclick', 'excluir('+tarefa.id+')')
+
+
+  div.appendChild(btnEditar)
+  div.appendChild(btnExcluir)
+  
+  li.appendChild(span)
+  li.appendChild(div)
+  return li
 };
 
-const limpaStorage = () => {
-  window.localStorage.clear();
-};
+const editar = idTarefa => alert(idTarefa)
+const excluir = idTarefa => alert(idTarefa)
 
-document.getElementById("submit").onclick = criaStorage;
-document.getElementById("remove-storage").onclick = removeStorage;
-document.getElementById("limpa-storage").onclick = limpaStorage;
-const input = document.getElementById("title-Todo");
-const textarea = document.getElementById("list-to-do");
-
-getStorage();
