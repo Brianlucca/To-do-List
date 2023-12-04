462e9bab900ac6834aaa58c011dc35dfdb1bc9f6
Brian Lucca <brianlucca2002@gmail.com>
2023-12-04 15:24:37 -0300

    Adicionando pagina editar

--- src/assents/js/index.js
+++ src/assents/js/index.js
@@ -7,17 +7,17 @@ input.addEventListener("keypress", (e) => {
     let tarefa = {
       nome: input.value,
       id: gerarID(),
-    }
-    adicionarLista(tarefa)
+    };
+    adicionarLista(tarefa);
   }
 });
 
-btn.addEventListener('click', (e) => {
+btn.addEventListener("click", (e) => {
   let tarefa = {
     nome: input.value,
     id: gerarID(),
-  }
-  adicionarLista(tarefa)
+  };
+  adicionarLista(tarefa);
 });
 
 const gerarID = () => Math.floor(Math.random() * 3000);
@@ -25,37 +25,52 @@ const gerarID = () => Math.floor(Math.random() * 3000);
 const adicionarLista = (tarefa) => {
   let li = criarTagLi(tarefa);
   resultado.appendChild(li);
-  input.value = ''
+  input.value = "";
 };
 
 const criarTagLi = (tarefa) => {
-  let li = document.createElement('li')
+  let li = document.createElement("li");
+  li.id = tarefa.id;
 
-  let span = document.createElement('span')
-  span.classList.add('text-list')
-  span.innerHTML = ' <input type="checkbox" id="meuCheckbox" name="meuCheckbox" value="aceito">'+ tarefa.nome
+  let span = document.createElement("span");
+  span.classList.add("text-list");
+  span.innerHTML =
+    ' <input type="checkbox" id="meuCheckbox" name="meuCheckbox" value="aceito">' +
+    tarefa.nome;
 
-  let div = document.createElement('div')
+  let div = document.createElement("div");
 
-  let btnEditar = document.createElement('button')
-  btnEditar.classList.add('btnAcao')
-  btnEditar.innerHTML = '<i class="fa fa-pencil"></i>'
-  btnEditar.setAttribute('onclick', 'editar('+tarefa.id+')')
+  let btnEditar = document.createElement("button");
+  btnEditar.classList.add("btnAcao");
+  btnEditar.innerHTML = '<i class="fa fa-pencil"></i>';
+  btnEditar.setAttribute("onclick", "editar(" + tarefa.id + ")");
 
-  let btnExcluir = document.createElement('button')
-  btnExcluir.classList.add('btnAcao')
-  btnExcluir.innerHTML = '<i class="fa fa-trash"></i>'
-  btnExcluir.setAttribute('onclick', 'excluir('+tarefa.id+')')
+  let btnExcluir = document.createElement("button");
+  btnExcluir.classList.add("btnAcao");
+  btnExcluir.innerHTML = '<i class="fa fa-trash"></i>';
+  btnExcluir.setAttribute("onclick", "excluir(" + tarefa.id + ")");
 
+  div.appendChild(btnEditar);
+  div.appendChild(btnExcluir);
 
-  div.appendChild(btnEditar)
-  div.appendChild(btnExcluir)
-  
-  li.appendChild(span)
-  li.appendChild(div)
-  return li
+  li.appendChild(span);
+  li.appendChild(div);
+  return li;
 };
 
-const editar = idTarefa => alert(idTarefa)
-const excluir = idTarefa => alert(idTarefa)
-
+const editar = (idTarefa) => {
+  alert(idTarefa);
+  let confirmacao =  window.confirm('Deseja editar essa Lista?')
+  if(confirmacao) {
+    
+  }
+};
+const excluir = (idTarefa) => {
+  let confirmacao =  window.confirm('Deseja excluir essa Lista?')
+  if(confirmacao) {
+    let li = document.getElementById('' +idTarefa + '')
+    if(li) {
+      resultado.removeChild(li)
+    }
+  }
+};
